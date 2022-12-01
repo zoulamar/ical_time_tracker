cd2c895febadb096ff68755d92cc56f39781abfe
m@mzt460s <m@mzt460s>
2022-12-01 11:31:13 +0100

    ~

--- report.py
+++ report.py
@@ -2,6 +2,10 @@
 
 import os
 
+month=11
+
 for i in range(1,31):
-    os.system(f"./main.py conf/dummy.yaml --since 2022-10-{i:02} --until 2022-10-{i+1:02} -t CRL -c")
+    os.system(f"./main.py conf/dummy.yaml --since 2022-{month}-{i:02} --until 2022-{month}-{i+1:02} -t CRL -c")
     print()
+
+print("BEWARE, THE LAST DAY NEEDS TO BE EXTRACTED MANUALLY")
